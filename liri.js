a5805af86d916b605a5057ee73be28ac646285bd
Kyle Ruff <kyleruff@gmail.com>
2019-07-20 13:48:50 -0600

    installed dotenv

---
+++ liri.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+
+//VARS
+var request = require("request");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+//vars to capture user inputs.
+var userOption = process.argv[2];
+var inputParameter = process.argv[3];
+
+//Execute function
+UserInputs(userOption, inputParameter);
+
+//FUNCTIONS
+function UserInputs(userOption, inputParameter) {
+    switch (userOption) {
+
+        case 'spotify-this-song':
+            showSongInfo(inputParameter);
+            break;
+
+
+        default:
+            console.log("Invalid Command")
+    }
+}
+//Funtion for Music Info: Spotify
+function showSongInfo(inputParameter) {
+    if (inputParameter === undefined) {
+        inputParameter = "The Sign"; //default Song
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: inputParameter
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log("**********SONG INFO*********");
+                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i + "\n");
+                console.log("Song name: " + songs[i].name);
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name + "\n");
+                console.log("Preview song: " + songs[i].preview_url);
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url + "\n");
+                console.log("Album: " + songs[i].album.name);
+                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                console.log("Artist(s): " + songs[i].artists[0].name);
+                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                console.log("*****************************");
+                fs.appendFileSync("log.txt", "*****************************\n");
+            }
+        }
+    );
+};
+//function for reading out of random.txt file  
+function showSomeInfo() {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var dataArr = data.split(',');
+        UserInputs(dataArr[0], dataArr[1]);
+    });
+}
